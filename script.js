7ae46989abd1b080676ed795ca4d0a2c0c048800
zuanexo <grooovydon@gmail.com>
2021-04-08 00:01:06 +0530

    second commit

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let container = document.querySelector('.container')
 let lastGridSize
+let color = "black"
+let greyLum =90
 
 
 function gridMaker(gridSize) {
@@ -11,7 +13,7 @@ function gridMaker(gridSize) {
         let div = document.createElement('div');
         div.classList.add('pixel');  
         div.style.border = "0.5px solid grey";
-        div.onmouseover = () => {div.style.backgroundColor = 'black'};
+        div.onmouseover = (e) => {div.style.backgroundColor = colorFunc(e)};
         container.appendChild(div);
     
     }
@@ -31,5 +33,41 @@ button.onclick = () =>{
         gridMaker(userGridInt)
     }
 }
+function colorFunc(e) {
+    if (color=="rand") { 
+       
+        let rc=`hsl(${Math.floor(Math.random()*360)},${Math.floor(Math.random()*80)+20}%,${Math.floor(Math.random()*50)+20}%)`
+        return (rc)
+    } else if(color=="black"){
+        return ("rgb(0, 0, 0)")
+    } else if(color=="grey") {
+        let bg=e.target.style.backgroundColor
+        if (bg.match(/rgba/)) {
+            let opacity = Number(bg.slice(-4,-1))
+            return(`rgba(0, 0, 0, ${opacity + 0.1})`);    
+        } else if (bg=="rgb(0, 0, 0)"){
+            return("rgb(0, 0, 0)")
+        } else {
+            return("rgba(0,0,0,0.1")
+        }
+    } else {
+        return color
+    }
+}
+
+
+//random color//
+let randColor = document.querySelector('#random')
+randColor.onclick= () =>{color="rand"}
+
+//black color//
+document.querySelector("#black").onclick=()=>{color="black"}
+
+//grey//
+document.querySelector("#grey").onclick=()=>{color="grey"}
+
+//custom//
+document.querySelector("#custom").onchange=(event)=>{color=event.target.value}
+
 
 gridMaker(16)
